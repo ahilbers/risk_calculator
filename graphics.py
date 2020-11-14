481c63536faf690c1d5183d1dc79e2d955a4f798
Adriaan P Hilbers <a.hilbers17@imperial.ac.uk>
2020-11-14 17:50:27 +0000

    initial commit

---
+++ graphics.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+
+def create_dice_plots():
+    for die_num in range(1, 7):        
+        fig, ax = plt.subplots(figsize=(2, 2), frameon=False)
+        ax.axis('off')
+        ax.add_patch(patches.Rectangle(
+            (0, 0), 1, 1, linewidth=0, facecolor='#eeeeee'
+        ))
+        if die_num in [1, 3, 5]:
+            ax.add_patch(patches.Circle(
+                (0.5, 0.5), radius=0.1, edgecolor=None, facecolor='#000000'
+            ))
+        if die_num in [2, 3, 4, 5, 6]:
+            ax.add_patch(patches.Circle(
+                (0.2, 0.8), radius=0.1, edgecolor=None, facecolor='#000000'
+            ))
+        if die_num in [2, 3, 4, 5, 6]:
+            ax.add_patch(patches.Circle(
+                (0.8, 0.2), radius=0.1, edgecolor=None, facecolor='#000000'
+            ))
+        if die_num in [4, 5, 6]:
+            ax.add_patch(patches.Circle(
+                (0.2, 0.2), radius=0.1, edgecolor=None, facecolor='#000000'
+            ))
+        if die_num in [4, 5, 6]:
+            ax.add_patch(patches.Circle(
+                (0.8, 0.8), radius=0.1, edgecolor=None, facecolor='#000000'
+            ))
+        if die_num in [6]:
+            ax.add_patch(patches.Circle(
+                (0.2, 0.5), radius=0.1, edgecolor=None, facecolor='#000000'
+            ))
+            ax.add_patch(patches.Circle(
+                (0.8, 0.5), radius=0.1, edgecolor=None, facecolor='#000000'
+            ))
+        ax.set_xlim([0, 1])
+        ax.set_ylim([0, 1])
+        plt.savefig('img/die_{}.png'.format(die_num), bbox_inches='tight',
+                    pad_inches=0)
+
+
+def make_legend():
+    """Make a legend for ACF plot."""
+    fig, ax = plt.subplots(figsize=(6.7, 0.5))
+    bbox_props = dict(boxstyle='round,pad=0.3', fc='none',
+                      ec='#c5c8c4', lw=1)
+    text = ('        k-medoids clustering            '
+            'importance subsampling            target')
+    ax.text(0.5, 0.5, text, ha='center', va='center',
+            size=10, bbox=bbox_props)
+
+    ax.add_patch(patches.Rectangle(
+        (0.41, 0.4), 0.05, 0.2, linewidth=0, facecolor='#000000'
+    ))
+    ax.axhline(y=0.5, xmin=0.84, xmax=0.89, color='k', linestyle='--')
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    ax.axis('off')
+    plt.savefig('outputs/legend.pdf', bbox_inches='tight', pad_inches=0)
+
+
+def main():
+    create_dice_plots()
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.js
@@ -0,0 +1,162 @@
+function isPositiveInt(mynumber) {
+    return (Number.isInteger(mynumber) && mynumber > 0);
+}
+
+
+function arePositiveInts(mynumber1, mynumber2) {
+    return (isPositiveInt(mynumber1) && isPositiveInt(mynumber2));
+}
+
+
+// Roll the dice for one round of battle
+function rollDice(unitsAttack, unitsDefend, roundInfoDict) {
+    // Determine the rolls for attacker and defender and sort them
+    // from highest to lowest
+    let rollsAttack = Array(unitsAttack).fill(0);
+    let rollsDefend = Array(unitsDefend).fill(0);
+    for (i=0; i<rollsAttack.length; i++)
+        rollsAttack[i] = Math.floor(Math.random() * 6) + 1;
+    for (i=0; i<rollsDefend.length; i++)
+        rollsDefend[i] = Math.floor(Math.random() * 6) + 1;
+    rollsAttack.sort().reverse();
+    rollsDefend.sort().reverse();
+
+    // Add the rolls to the info about the round
+    roundInfoDict.rollsAttack = rollsAttack;
+    roundInfoDict.rollsDefend = rollsDefend;
+
+    return [rollsAttack, rollsDefend];
+}
+
+
+// Determine the losses from one round of battle
+function battleOneRound(unitsAttack, unitsDefend, roundInfoDict) {
+
+    let [rollsAttack, rollsDefend] = rollDice(unitsAttack, unitsDefend,
+                                              roundInfoDict);
+
+    // Determine the losses to both the attacker and defender
+    let lossAttack = 0;
+    let lossDefend = 0;
+    for (i=0; i<Math.min(rollsAttack.length, rollsDefend.length); i++) {
+        if (rollsAttack[i] > rollsDefend[i])
+            lossDefend++;
+        else
+            lossAttack++;
+    }
+
+    return [lossAttack, lossDefend];
+}
+
+
+function getDieImage(roll) {
+    let DOM_img = document.createElement("img");
+    // DOM_img.src = "img/die_1.png"
+    DOM_img.src = "img/die_" + roll.toString() + ".png";
+    DOM_img.height = 50;
+    DOM_img.width = 50;
+    DOM_img.hspace = 5;
+    DOM_img.vspace = 5;
+    return DOM_img
+}
+
+
+function renderRoundInfo(roundInfoDict) {
+    d = roundInfoDict;    // Makes code more concise
+    par = document.createElement("p");
+
+    par.appendChild(document.createTextNode("Round number "+d.roundNumber));
+    par.appendChild(document.createElement("br"));
+    par.appendChild(document.createTextNode("Units left: "
+                                            + d.unitsAttackLeftAtStart + " "
+                                            + d.unitsDefendLeftAtStart));
+    par.appendChild(document.createElement("br"));
+    par.appendChild(document.createTextNode("Units used: "
+                                            + d.unitsAttackRound + " "
+                                            + d.unitsDefendRound));
+    par.appendChild(document.createElement("br"));
+    par.appendChild(document.createTextNode("Rolls attack: "
+                                            + d.rollsAttack));
+    par.appendChild(document.createElement("br"));
+    for (i=0; i<d.rollsAttack.length; i++) {
+        par.appendChild(getDieImage(d.rollsAttack[i]));
+    }
+    par.appendChild(document.createElement("br"));
+    par.appendChild(document.createTextNode("Rolls defend: "
+                                            + d.rollsDefend));
+    par.appendChild(document.createElement("br"));
+    for (i=0; i<d.rollsDefend.length; i++) {
+        par.appendChild(getDieImage(d.rollsDefend[i]));
+    }
+    par.appendChild(document.createElement("br"));
+    par.appendChild(document.createTextNode("Units lost: "
+                                            + d.lossAttack + " "
+                                            + d.lossDefend));
+    par.appendChild(document.createElement("br"));
+    par.appendChild(document.createTextNode("Units left: "
+                                            + d.unitsAttackLeftAtEnd + " "
+                                            + d.unitsDefendLeftAtEnd));
+    par.appendChild(document.createElement("br"));
+    document.getElementById("div1").appendChild(par);
+    
+}
+
+
+function battleUntilEnd(unitsAttackTotal, unitsDefendTotal) {
+
+    if (!arePositiveInts(unitsAttackTotal, unitsDefendTotal))
+        return "[ERROR] Both player units should be positive whole numbers.";
+
+    // Create new variables (*not* references to the same object -- these
+    // are different variables) that track the amount of units left
+    let unitsAttackLeft = unitsAttackTotal;
+    let unitsDefendLeft = unitsDefendTotal;
+    let lossAttack;
+    let lossDefend;
+    let roundNumber = 1;
+    let par;
+
+    while (Math.min(unitsAttackLeft, unitsDefendLeft) > 0) {
+        let roundInfoDict = {};
+        roundInfoDict.roundNumber = roundNumber;
+        roundInfoDict.unitsAttackLeftAtStart = unitsAttackLeft;
+        roundInfoDict.unitsDefendLeftAtStart = unitsDefendLeft;
+
+        // Do the actual battle
+        let unitsAttackRound = Math.min(unitsAttackLeft, 3);
+        let unitsDefendRound = Math.min(unitsDefendLeft, 2);
+        [lossAttack, lossDefend] = battleOneRound(unitsAttackRound,
+                                                  unitsDefendRound,
+                                                  roundInfoDict);
+        unitsAttackLeft -= lossAttack;
+        unitsDefendLeft -= lossDefend;
+        roundNumber++;
+
+        // Add info to the dictionary summarising this round
+        roundInfoDict.unitsAttackRound = unitsAttackRound;
+        roundInfoDict.unitsDefendRound = unitsDefendRound;
+        roundInfoDict.lossAttack = lossAttack;
+        roundInfoDict.lossDefend = lossDefend;
+        roundInfoDict.unitsAttackLeftAtEnd = unitsAttackLeft;
+        roundInfoDict.unitsDefendLeftAtEnd = unitsDefendLeft;
+
+        renderRoundInfo(roundInfoDict);
+
+        console.log(roundInfoDict);
+    }
+
+    return 0;
+}
+
+
+function battleFromBattleButton() {
+    let unitsAttackTotal = (
+        Number(document.getElementById("in_attack_units").value)
+    );
+    let unitsDefendTotal = (
+        Number(document.getElementById("in_defend_units").value)
+    );
+    let output = battleUntilEnd(unitsAttackTotal, unitsDefendTotal);
+
+    console.log(0);
+}
